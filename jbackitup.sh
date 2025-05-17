975b5b38cb96a678b3a02ee8a4957684d0d4cad5
Jim Willeke <jim@willeke.com>
2025-05-17 04:12:24 -0400

    Update version

--- jbackitup.sh
+++ jbackitup.sh
@@ -3,7 +3,7 @@
 # SCRIPT: jbackup.sh
 # AUTHOR: jim@willeke.com
 # DATE:   2013-02-16
-T_VER=1.1A								# Script Version Number
+T_VER=2025-05-04							# Script Version Number
 # REV:    1.1.A (Valid are A, B, D, T, Q, and P)
 #               (For Alpha, Beta, Dev, Test, QA, and Production)
 #
@@ -59,8 +59,14 @@ LOGFILE="$LOGDIR/`date +%Y-%m-%d`-$LOGFILENAME"
 EMAIL_NOTIFY="systems@willeke.com"
 EMAIL_FROM_ADDRESS="systems@willeke.com"
 EMAIL_FROM="$APPHOST"
-SOURCE="/home"
-TARGET="/var/local/backup"
+# Setting backup destination directory
+BACKUP_DIR="/var/local/backup"
+# Defining an array of source directories to back up
+declare -a SOURCE_DIRS=("/path/to/folder1" "/path/to/folder2" "/path/to/folder3")
+# Setting backup destination directory
+BACKUP_DIR="/path/to/backup/destination"
+# Creating timestamp for backup files
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 ##########################################################
 #              DEFINE FUNCTIONS HERE
 ##########################################################
@@ -139,39 +145,44 @@ f_messagesend()
 ##########################################################
 f_lock_file()
 {
-   if [ -f ${TARGET}/lockfile ]
+  l_target=$1
+
+   if [ -f ${l_target}/lockfile ]
    then
       echo "Lockfile exists, backup stopped."
-      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Lockfile ${TARGET}/lockfile exists, backup stopped." 
+      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Lockfile ${l_target}/lockfile exists, backup stopped." 
       exit 2
    else
-      touch ${TARGET}/lockfile
+      touch ${l_target}/lockfile
    fi
 }
 ##########################################################
 # create folders if neccessary
+# $1 = folder name
 ##########################################################
 f_create_folders()
 {
-   if [ ! -e ${TARGET}/current ]
-   then
-      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: mkdir ${TARGET}/current" 
-      mkdir ${TARGET}/current
+  l_folder=$1
+
+  if [ ! -e ${l_folder} ]
+  then
+      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: mkdir ${l_folder}"
+      mkdir ${l_folder}
+  fi
+  if [ ! -d ${l_folder}/weekly ]
+  then
+      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: mkdir ${l_folder}/weekly" 
+      mkdir ${l_folder}/weekly
    fi
-   if [ ! -d ${TARGET}/weekly ]
+   if [ ! -d ${l_folder}/daily ]
    then
-      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: mkdir ${TARGET}/weekly" 
-      mkdir ${TARGET}/weekly
+      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: mkdir ${l_folder}/daily" 
+      mkdir ${l_folder}/daily
    fi
-   if [ ! -d ${TARGET}/daily ]
+   if [ ! -d ${l_folder}/hourly ]
    then
-      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: mkdir ${TARGET}/daily" 
-      mkdir ${TARGET}/daily
-   fi
-   if [ ! -d ${TARGET}/hourly ]
-   then
-      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: mkdir ${TARGET}/hourly" 
-      mkdir ${TARGET}/hourly
+      f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: mkdir ${l_folder}/hourly" 
+      mkdir ${l_folder}/hourly
    fi
 }
 ##########################################################
@@ -182,55 +193,54 @@ f_write_and_log "$LAGS "
 f_write_and_log "$APPHOST  $THIS_SCRIPT - see - $LOGFILE"
 f_write_and_log "$DOUBLEBAR"
 f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: $0 $APPNAME Script Name: $THIS_SCRIPT Script Version $T_VER Started by $USER"
-if [ "$1" != "" ]
-then
-   f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Source specified as $1"    
-   SOURCE=$1
-else
-   f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: No Source specified using $SOURCE"
-fi
-f_lock_file
-f_create_folders
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: $0 $APPNAME $THIS_SCRIPT SOURCE: $SOURCE is being backed up to $TARGET"
-# rsync
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: rsync --archive --xattrs --verbose --human-readable --delete --exclude-from='/root/bin/rsync-exclude.txt' --link-dest=${TARGET}/current $SOURCE $TARGET/$DATETIME-incomplete"
-f_cmd rsync --archive --xattrs --verbose --human-readable --delete --exclude-from='/root/bin/rsync-exclude.txt' --link-dest=${TARGET}/current $SOURCE $TARGET/$DATETIME-incomplete
-# backup complete
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Backup Complete! "
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Move $TARGET/$DATETIME-incomplete to ${TARGET}/hourly/$DATETIME"
-mv $TARGET/$DATETIME-incomplete ${TARGET}/hourly/$DATETIME
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Removing ${TARGET}/current" 
-rm -r ${TARGET}/current
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Link into ln -s ${TARGET}/hourly/$DATETIME ${TARGET}/current" 
-ln -s ${TARGET}/hourly/$DATETIME ${TARGET}/current
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Set Date ${TARGET}/hourly/$DATETIME"
-touch ${TARGET}/hourly/$DATETIME
+# Ensuring backup directory exists
+f_create_folders "$BACKUP_DIR"
+f_lock_file "$BACKUP_DIR"
+# Looping through each source directory
+for DIR in "${SOURCE_DIRS[@]}"; do
+    # Getting the base name of the directory
+    DIR_NAME=$(basename "$DIR")
+    # Creating tar file name
+    TAR_FILE="$BACKUP_DIR/${TIMESTAMP}_${DIR_NAME}.tar.gz"
+    f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: $0 $APPNAME $THIS_SCRIPT SOURCE: $DIR_NAME is being backed up to $TAR_FILE"
+    # Creating tar archive
+    tar -czf "$TAR_FILE" -C "$(dirname "$DIR")" "$DIR_NAME"
+    # Checking if tar command was successful
+    if [ $? -eq 0 ]; then
+        f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`:Backup of $DIR completed: $TAR_FILE"
+    else
+        f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: ERROR: Backup of $DIR failed"
+    fi
+done
 
+f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: All Backup Complete!"
+f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Delete old backups"
+find "$BACKUP_DIR" -name "*.tar.gz" -mtime +13 -delete
 # keep daily backup
-if [ `find ${TARGET}/daily -maxdepth 1 -type d -mtime -2 -name "20*" | wc -l` -eq 0 ] && [ `find ${TARGET}/hourly -maxdepth 1 -name "20*" | wc -l` -gt 1 ]
-then
-   oldest=`ls -1 -tr ${TARGET}/hourly/ | head -1`
-   f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Keep daily backups  mv ${TARGET}/hourly/$oldest ${TARGET}/daily/"   
-   mv ${TARGET}/hourly/$oldest ${TARGET}/daily/
-fi
+
+#if [ `find ${TARGET}/daily -maxdepth 1 -type d -mtime -2 -name "20*" | wc -l` -eq 0 ] && [ `find ${TARGET}/hourly -maxdepth 1 -name "20*" | wc -l` -gt 1 ]
+#then
+#   oldest=`ls -1 -tr ${TARGET}/hourly/ | head -1`
+#   f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Keep daily backups  mv ${TARGET}/hourly/$oldest ${TARGET}/daily/"   
+#   mv ${TARGET}/hourly/$oldest ${TARGET}/daily/
+#fi
 
 # keep weekly backup
-
-if [ `find ${TARGET}/weekly -maxdepth 1 -type d -mtime -14 -name "20*" | wc -l` -eq 0 ] && [ `find ${TARGET}/daily -maxdepth 1 -name "20*" | wc -l` -gt 1 ]
-then
-   oldest=`ls -1 -tr ${TARGET}/daily/ | head -1`
-   f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Keep weekly backups  mv ${TARGET}/daily/$oldest ${TARGET}/weekly/"   
-   mv ${TARGET}/daily/$oldest ${TARGET}/weekly/
-fi
+#if [ `find ${TARGET}/weekly -maxdepth 1 -type d -mtime -14 -name "20*" | wc -l` -eq 0 ] && [ `find ${TARGET}/daily -maxdepth 1 -name "20*" | wc -l` -gt 1 ]
+#then
+#   oldest=`ls -1 -tr ${TARGET}/daily/ | head -1`
+#   f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Keep weekly backups  mv ${TARGET}/daily/$oldest ${TARGET}/weekly/"   
+#   mv ${TARGET}/daily/$oldest ${TARGET}/weekly/
+#fi
 
 # delete old backups
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Delete old backups"
-find ${TARGET}/hourly -maxdepth 1 -type d -mtime +0 | xargs rm -rf
-find ${TARGET}/daily -maxdepth 1 -type d -mtime +7 | xargs rm -rf
+#f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Delete old backups"
+#find ${TARGET}/hourly -maxdepth 1 -type d -mtime +0 | xargs rm -rf
+#find ${TARGET}/daily -maxdepth 1 -type d -mtime +7 | xargs rm -rf
 
 # remove lockfile
-f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Remove:  ${TARGET}/lockfile"
-rm ${TARGET}/lockfile
+f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: Remove:  ${BACKUP_DIR}/lockfile"
+rm ${BACKUP_DIR}/lockfile
 f_write_and_log "`date '+%Y-%m-%d %H:%M:%S'`: $0 $APPNAME Script Name: $THIS_SCRIPT Script Version $T_VER exited by $USER"
 
 ##########################################################
